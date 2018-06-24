29b8b68ca6bd392798835f6d8a28abc3aadd5d15
Yeonchan <acha21@europa.snu.ac.kr>
2018-06-24 13:44:41 +0900

    Update go.sh

--- go.sh
+++ go.sh
@@ -2,6 +2,8 @@ sudo sed -i 's/kr.archive.ubuntu.com/ftp.daumkakao.com/g' /etc/apt/sources.list
 
 sudo apt-get install zsh
 
+apt-get install git-core
+
 for cmd in git vim zsh tmux
 do
     if ! type $cmd > /dev/null; then
