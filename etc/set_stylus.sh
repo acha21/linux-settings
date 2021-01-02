a6772c7f84fdb15437a4020571fc979f014eced6
Yeonchan <acha21@europa.snu.ac.kr>
2021-01-02 23:31:08 +0900

    added stylus setup and screen setup

---
+++ etc/set_stylus.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xsetwacom set "HUION Huion Tablet_H640P Pad pad" button 1 key +ctrl +shift +P
+xsetwacom set "HUION Huion Tablet_H640P Pad pad" button 2 key +ctrl +shift +H
+xsetwacom set "HUION Huion Tablet_H640P Pad pad" button 3 key +ctrl +shift +E
