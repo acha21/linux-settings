d8acd92e1ba7a683f3085e63cfff70c215454e2a
Yeonchan <acha21@europa.snu.ac.kr>
2021-01-16 17:02:47 +0900

    set_stylus update

--- etc/set_stylus.sh
+++ etc/set_stylus.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 xsetwacom set "HUION Huion Tablet_H640P Pad pad" button 1 key +ctrl +shift +P
 xsetwacom set "HUION Huion Tablet_H640P Pad pad" button 2 key +ctrl +shift +H
-xsetwacom set "HUION Huion Tablet_H640P Pad pad" button 3 key +ctrl +shift +L
+xsetwacom set "HUION Huion Tablet_H640P Pad pad" button 3 key +ctrl +shift +S
+
+echo "HUION tablet's key is setup"
