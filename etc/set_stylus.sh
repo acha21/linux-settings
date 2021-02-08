4426209f16d317de83dd4304e63135beaf461b4b
Yeonchan <acha21@europa.snu.ac.kr>
2021-02-08 09:35:31 +0900

    rotate stylus

--- etc/set_stylus.sh
+++ etc/set_stylus.sh
@@ -2,5 +2,6 @@
 xsetwacom set "HUION Huion Tablet_H640P Pad pad" button 1 key +ctrl +shift +P
 xsetwacom set "HUION Huion Tablet_H640P Pad pad" button 2 key +ctrl +shift +H
 xsetwacom set "HUION Huion Tablet_H640P Pad pad" button 3 key +ctrl +shift +S
+xsetwacom set "HUION Huion Tablet_H640P stylus" Rotate half
 
 echo "HUION tablet's key is setup"
